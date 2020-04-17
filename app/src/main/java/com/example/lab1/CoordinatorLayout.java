2032f5cf0afd9dee3f2d8d724c61bc6a5dde5004
Dmitrii <kislenko1910@mail.ru>
2020-04-17 11:14:14 +0300

    Fixed a bug with coordinator layout

--- app/src/main/java/com/example/lab1/CoordinatorLayout.java
+++ app/src/main/java/com/example/lab1/CoordinatorLayout.java
@@ -11,7 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class CoordinatorLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +24,7 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Here will be some action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -32,16 +32,12 @@ public class ScrollingActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_scrolling, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
